51f3ea92c7f4f944e38bfb1984fdeb59a42d540d
yulppva <it@yulppva.ru>
2024-01-21 14:39:10 +0300

    Add save, get, delete, clear, getAll, size methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,54 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private static final int STORAGE_SIZE = 10000;
+    Resume[] storage = new Resume[STORAGE_SIZE];
+    int size = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        size = 0;
     }
 
     void save(Resume r) {
+        if (getIndex(r.uuid) == -1 && size < STORAGE_SIZE) {
+            storage[size++] = r;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int i = getIndex(uuid);
+        return i >= 0 ? storage[i] : null;
     }
 
     void delete(String uuid) {
+        int i = getIndex(uuid);
+        if (i >= 0) {
+            storage[i] = storage[size - 1];
+            storage[--size] = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- src/MainArray.java
+++ src/MainArray.java
@@ -10,10 +10,12 @@ public class MainArray {
     private final static ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | size | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | size | save " +
+                    "uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
